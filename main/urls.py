9a419994f757214962eb3763c0db92a38683e935
Pricll <spolishuk221@gmail.com>
2024-03-23 23:42:19 +0100

    Added goods app, conected catalog & poduct html

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 
+app_name = 'main'
 urlpatterns = [
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
